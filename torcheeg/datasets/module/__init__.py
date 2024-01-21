76a7f47832cac587c544ec79e258f744e4e7b211
Chang717 <1252479770@qq.com>
2024-01-21 17:47:30 +0800

    Typo ssvep

--- torcheeg/datasets/module/__init__.py
+++ torcheeg/datasets/module/__init__.py
@@ -2,7 +2,7 @@ from .base_dataset import BaseDataset
 from .folder_dataset import FolderDataset
 from .csv_folder_dataset import CSVFolderDataset
 
-from .sspev import *
+from .ssvep import *
 from .motor_imagery import *
 from .emotion_recognition import *
 from .personal_identification import *
